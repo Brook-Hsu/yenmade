8e3157859dee8821cf70f80394e69cdb2f7541ea
Brook-Hsu <dinohsu7@gmail.com>
2025-10-05 00:48:07 +0800

    加上輪播

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper(".productSwiper", {
   // 輪播循環
   loop: true,
+    autoplay: {
+    delay: 1500, // 每秒切換一次（單位：毫秒）
+    disableOnInteraction: false, // 使用者滑動後仍會繼續自動播放
+    pauseOnMouseEnter: true, // 新版 Swiper v9+ 可用此屬性
+  },
   // 手機板
   slidesPerView: 1.8,
   spaceBetween: 16,
